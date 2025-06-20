6a190c18a812e7fe426b89860d9fa1819f8890fa
Pamela Fox <pamela.fox@gmail.com>
2025-06-20 17:38:05 +0000

    Separate server from build

--- build.sh
+++ build.sh
@@ -2,4 +2,3 @@ python3 -m jinja2ssg --src content --dest build build
 cp -r content/images build/
 cp -r content/favicon.ico build/
 cp -r content/lectures/media build/lectures/
-python3 -m http.server 8000 --directory build
