14baa5f8398f4ba792d28de1b04de975da4e5835
Pamela Fox <pamela.fox@gmail.com>
2025-06-20 08:42:04 -0700

    Rename articles folder, add lecture slides (#7)

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-python3 -m jinja2ssg --src articles --dest docs build
-cp -r articles/images docs/
-cp -r articles/favicon.ico docs/
+python3 -m jinja2ssg --src content --dest docs build
+cp -r content/images docs/
+cp -r content/favicon.ico docs/
+cp -r content/lectures/media docs/lectures/
 python3 -m http.server 8000 --directory docs
